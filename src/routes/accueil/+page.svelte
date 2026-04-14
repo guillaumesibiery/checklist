dc80db7dd8e82093c8d7fe2a8670b1d6a5bd82bf
Guillaume Sibiery <guillaume.sibiery@gmail.com>
2026-04-14 20:59:08 +0200

    Modify labels

--- src/routes/accueil/+page.svelte
+++ src/routes/accueil/+page.svelte
@@ -46,9 +46,9 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0c0 .414-.336.75-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
                             </svg>
                         </div>
-                        <p class="text-gray-500 font-medium text-center">Tu n'as aucune checklist en cours pour le moment.</p>
+                        <p class="text-gray-500 font-medium text-center">Aucune checklist en cours pour le moment.</p>
                         <button class="mt-6 px-6 py-3 bg-primary text-text-inverse font-bold rounded-2xl shadow-lg shadow-primary/20 hover:opacity-90 active:scale-95 transition-all cursor-pointer">
-                            En créer une maintenant
+                            Créer une checklist
                         </button>
                     </div>
                 </div>
